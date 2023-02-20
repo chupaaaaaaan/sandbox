293bb7ec37871f9c264c5f69f429a055a3095ee2
Takayuki Uchida <jacobitebeerlove@gmail.com>
2023-02-20 23:38:36 +0900

    設定の取得処理を分離/デフォルト値と環境変数からの取得をサポート

--- get-payslip/src/Run.hs
+++ get-payslip/src/Run.hs
@@ -10,32 +10,24 @@ import Import
 import Scraper
 import Util
 import RIO.Process
-import qualified RIO.Map as Map
 import Control.Monad.Cont
-import RIO.Text (unpack)
 
 run :: (HasLogFunc env, HasProcessContext env) => RIO env ()
 run = do
-    -- content <- scrapeContent <$> readFileUtf8 "./test.html"
-    envMap <- view envVarsL
 
-    let lu = flip Map.lookup envMap
-        mPassword        = lu "GET_PAYSLIP_PASSWORD"
-        mHost            = lu "GET_PAYSLIP_HOST" <&> unpack
-        mPort            = lu "GET_PAYSLIP_PORT" >>= readMaybe . unpack
-        mDownloadBaseDir = lu "GET_PAYSLIP_DOWNLOAD_BASEDIR" <&> unpack
-    
-    (`runContT` return) $ do 
-        password <- mPassword !? "Environment variable `GET_PAYSLIP_PASSWORD' is not set."
-        host <- mHost !? "Environment variable `GET_PAYSLIP_HOST' is not set."
-        port <- mPort !? "Environment variable `GET_PAYSLIP_PORT' is not set or is not Int."
-        downloadBaseDir <- mDownloadBaseDir !? "Environment variable `GET_PAYSLIP_DOWNLOAD_BASEDIR' is not set or is not Int."
+    mAppConfig <- getAppConfig
+    (`runContT` return) $ do
+        appConfig <- mAppConfig !? "Environment variable `GET_PAYSLIP_PASSWORD' is not set."
+        let password = runIdentity appConfig.targetUrlPassword
+            host = runIdentity appConfig.webdriverHost
+            port = runIdentity appConfig.webdriverPort
+            baseDir = runIdentity appConfig.downloadBaseDir
         mContent <- liftIO $ scrapeContent <$> fetchContentPage host port password
         content <- mContent !? "Cannot get the target content from the page."
 
         logInfo $ display content._url
         -- logInfo $ display content._title
-        liftIO $ downloadZip downloadBaseDir content._url
+        liftIO $ downloadZip baseDir content._url
 
     where
         Nothing !? e = ContT $ const $ logWarn e
--- get-payslip/src/Types.hs
+++ get-payslip/src/Types.hs
@@ -1,33 +1,88 @@
 {-# LANGUAGE NoImplicitPrelude #-}
-module Types
-  ( App (..)
-  , Options (..)
-  ) where
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingVia #-}
+{-# LANGUAGE StrictData #-}
 
+module Types (
+    App (..),
+    Options (..),
+    AppConfig (..),
+    getAppConfig,
+) where
+
+import Barbies
+import Barbies.Bare
+import Data.Monoid
 import RIO
+import RIO.Map qualified as Map
 import RIO.Process
+import RIO.Text
+import Data.Semigroup.Generic
+import Data.Monoid.Generic
+
+data AppConfig t f = AppConfig_
+    { targetUrlPassword :: Wear t f Text
+    , webdriverHost :: Wear t f String
+    , webdriverPort :: Wear t f Int
+    , downloadBaseDir :: Wear t f FilePath
+    }
+    deriving (Generic)
+
+instance FunctorB (AppConfig Covered)
+instance TraversableB (AppConfig Covered)
+instance ConstraintsB (AppConfig Covered)
+instance BareB AppConfig
+
+deriving via GenericSemigroup (AppConfig Covered f) instance AllBF Semigroup f (AppConfig Covered) => Semigroup (AppConfig Covered f)
+deriving via GenericMonoid (AppConfig Covered f) instance AllBF Monoid f (AppConfig Covered) => Monoid (AppConfig Covered f)
+
+defaultAppConfig :: RIO env (AppConfig Covered Last)
+defaultAppConfig =
+    return $
+        AppConfig_
+            { targetUrlPassword = Last Nothing
+            , webdriverHost = Last (Just "localhost")
+            , webdriverPort = Last (Just 4444)
+            , downloadBaseDir = Last (Just "./")
+            }
+
+appConfigFromEnvironment :: HasProcessContext env => RIO env (AppConfig Covered Last)
+appConfigFromEnvironment = do
+    envMap <- view envVarsL
+    let lu = flip Map.lookup envMap
+    return $
+        AppConfig_
+            { targetUrlPassword = Last $ lu "GET_PAYSLIP_PASSWORD"
+            , webdriverHost = Last $ lu "GET_PAYSLIP_HOST" <&> unpack
+            , webdriverPort = Last $ lu "GET_PAYSLIP_PORT" >>= readMaybe . unpack
+            , downloadBaseDir = Last $ lu "GET_PAYSLIP_DOWNLOAD_BASEDIR" <&> unpack
+            }
+
+getAppConfig :: HasProcessContext env => RIO env (Maybe (AppConfig Covered Identity))
+getAppConfig = do
+    fromDefault <- defaultAppConfig
+    fromEnvironment <- appConfigFromEnvironment
+
+    return $ getLast $ bsequence' $ fromDefault <> fromEnvironment
 
 -- | Command line arguments
 data Options = Options
-  { optionsVerbose :: !Bool
-  }
-
-data AppWDConfig = AWDConfig
-    { wdHost :: String
-    , wdPort :: Int
+    { optionsVerbose :: Bool
     }
 
-
-
 data App = App
-  { appLogFunc :: !LogFunc
-  , appProcessContext :: !ProcessContext
-  , appOptions :: !Options
-  -- , appWDConfig :: !AppWDConfig
-  -- Add other app-specific configuration information here
-  }
+    { appLogFunc :: LogFunc
+    , appProcessContext :: ProcessContext
+    , appOptions :: Options
+    -- Add other app-specific configuration information here
+    }
 
 instance HasLogFunc App where
-  logFuncL = lens appLogFunc (\x y -> x { appLogFunc = y })
+    logFuncL = lens appLogFunc (\x y -> x{appLogFunc = y})
 instance HasProcessContext App where
-  processContextL = lens appProcessContext (\x y -> x { appProcessContext = y })
+    processContextL = lens appProcessContext (\x y -> x{appProcessContext = y})
