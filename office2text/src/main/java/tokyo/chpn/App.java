84b754bb8eec66abba8932334c2554a04db69d6e
Takayuki Uchida <jacobitebeerlove@gmail.com>
2024-06-01 10:02:57 +0900

    create office2text

---
+++ office2text/src/main/java/tokyo/chpn/App.java
@@ -0,0 +1,23 @@
+package tokyo.chpn;
+
+import java.util.Set;
+import java.util.function.Predicate;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+
+
+
+
+
+    public static void main( String[] args ) {
+
+
+
+
+    }
+}
---
+++ office2text/src/test/java/tokyo/chpn/searcher/XlsxExtractorTest.java
@@ -0,0 +1,42 @@
+package tokyo.chpn.searcher;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.function.Predicate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class XlsxExtractorTest {
+
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    public void searchTextFromExcel() {
+        ClassLoader classLoader = getClass().getClassLoader();
+        String targetFileName = classLoader.getResource("data/ui-standard-parts-catalog.xlsx").getPath();
+        Set<Predicate<String>> predicates = new HashSet<>();
+
+        predicates.add(line -> line.contains("Semantic"));
+
+        XlsxExtractor extractor = new XlsxExtractor(targetFileName, predicates);
+
+        List<String> result = extractor.searchOnExcel();
+
+        assertEquals(2, result.size());
+    }
+
+
+
+}
