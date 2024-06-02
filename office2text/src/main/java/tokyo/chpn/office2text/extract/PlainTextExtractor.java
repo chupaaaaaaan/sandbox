3de4f4a18a7f535c1ddd0a2671590ba772d6f11e
Takayuki Uchida <jacobitebeerlove@gmail.com>
2024-06-03 01:17:26 +0900

    PlainTextExtractorをfinal化

--- office2text/src/main/java/tokyo/chpn/office2text/extract/PlainTextExtractor.java
+++ office2text/src/main/java/tokyo/chpn/office2text/extract/PlainTextExtractor.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
 
-public class PlainTextExtractor {
+public final class PlainTextExtractor {
 
     private PlainTextExtractor() {
     }
